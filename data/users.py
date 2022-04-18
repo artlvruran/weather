97e9591ec123232d63bd2e1c5aba6744e2dfc4d4
Кирилл <marincyrill@yandex.ru>
2022-04-18 22:39:58 +0500

    make it better

--- data/users.py
+++ data/users.py
@@ -1,7 +1,6 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
-from flask import url_for
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
--- forms/user.py
+++ forms/user.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
 from wtforms.validators import DataRequired
-from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class RegisterForm(FlaskForm):
