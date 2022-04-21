4636e2d9a1ceb4a6c9dfe4a84a6fec5a4fdd8c8a
Кирилл <marincyrill@yandex.ru>
2022-04-21 20:36:29 +0500

    последние штрихи

--- main.py
+++ main.py
@@ -104,5 +104,6 @@ def user(username):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
+    app.run(port=8080, host='127.0.0.1')
